24fb2fc247897f078b7baa695314c753c8e49024
Andrew Koh <andrew@eximchain.com>
2018-05-15 11:55:15 -0400

    changed to previous commits

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,44 +1,56 @@
 #!/bin/bash
+
 set -eu -o pipefail
 
 # install build deps
 add-apt-repository ppa:ethereum/ethereum
 apt-get update
-apt-get install -y build-essential unzip libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev solc sysvbanner wrk
+apt-get install -y build-essential unzip libdb-dev libsodium-dev zlib1g-dev libtinfo-dev cmake sysvbanner wrk libboost-all-dev
 
 # install constellation
-CVER="0.3.2"
-CREL="constellation-$CVER-ubuntu1604"
-wget -q https://github.com/jpmorganchase/constellation/releases/download/v$CVER/$CREL.tar.xz
-tar xfJ $CREL.tar.xz
-cp $CREL/constellation-node /usr/local/bin && chmod 0755 /usr/local/bin/constellation-node
-rm -rf $CREL
+wget -q https://github.com/jpmorganchase/constellation/releases/download/v0.0.1-alpha/ubuntu1604.zip
+unzip ubuntu1604.zip
+cp ubuntu1604/constellation-node /usr/local/bin && chmod 0755 /usr/local/bin/constellation-node
+cp ubuntu1604/constellation-enclave-keygen /usr/local/bin && chmod 0755 /usr/local/bin/constellation-enclave-keygen
+rm -rf ubuntu1604.zip ubuntu1604
+
+VER=v0.4.19
+DIR_NAME=solidity_0.4.19
+
+wget https://github.com/ethereum/solidity/releases/download/$VER/$DIR_NAME.tar.gz
+tar -xzf $DIR_NAME.tar.gz
+cd $DIR_NAME
+mkdir build
+cd build
+cmake .. && make
+# wget https://github.com/ethereum/solidity/releases/download/v0.4.19/solc-static-linux
+sudo mv solc/solc /usr/local/bin/
+sudo chmod 0755 /usr/local/bin/solc
+cd ../..
+# && rm -rf $DIR_NAME.tar.gz $DIR_NAME
 
 # install golang
-GOREL=go1.9.3.linux-amd64.tar.gz
-wget -q https://dl.google.com/go/$GOREL
+GOREL=go1.7.3.linux-amd64.tar.gz
+wget -q https://storage.googleapis.com/golang/$GOREL
 tar xfz $GOREL
 mv go /usr/local/go
 rm -f $GOREL
 PATH=$PATH:/usr/local/go/bin
-echo 'PATH=$PATH:/usr/local/go/bin' >> /home/vagrant/.bashrc
+echo 'PATH=$PATH:/usr/local/go/bin' >> /home/ubuntu/.bashrc
 
 # make/install quorum
-git clone https://github.com/jpmorganchase/quorum.git
+RELEASE="v1.2.1-modified"
+git clone https://github.com/Eximchain/quorum.git
 pushd quorum >/dev/null
-git checkout tags/v2.0.1
+git checkout $RELEASE
 make all
 cp build/bin/geth /usr/local/bin
 cp build/bin/bootnode /usr/local/bin
 popd >/dev/null
 
-# install Porosity
-wget -q https://github.com/jpmorganchase/quorum/releases/download/v1.2.0/porosity
-mv porosity /usr/local/bin && chmod 0755 /usr/local/bin/porosity
-
 # copy examples
 cp -r /vagrant/examples /home/vagrant/quorum-examples
-chown -R vagrant:vagrant /home/vagrant/quorum /home/vagrant/quorum-examples
+chown -R ubuntu:ubuntu ~/quorum /home/vagrant/quorum-examples
 
 # done!
 banner "Quorum"
