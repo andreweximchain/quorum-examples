996192914d71d1047917d63b152be41e886dd660
Andrew Koh <andrew@eximchain.com>
2018-05-25 13:54:10 -0400

    script1 checks for transaction gasPrice

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -23,18 +23,18 @@ rm -rf ubuntu1604.zip ubuntu1604
 # wget https://github.com/ethereum/solidity/archive/v0.4.5.tar.gz
 # tar -xzf $VER.tar.gz
 # cd $DIR_NAME
-git clone https://github.com/ethereum/solidity
-cd solidity
-git checkout b318366e
-git submodule update --init --recursive
-scripts/install_deps.sh
-mkdir build
-cd build
-cmake .. && make
-# wget https://github.com/ethereum/solidity/releases/download/v0.4.19/solc-static-linux
-sudo mv solc/solc /usr/local/bin/
-sudo chmod 0755 /usr/local/bin/solc
-cd ../..
+# git clone https://github.com/ethereum/solidity
+# cd solidity
+# git checkout b318366e
+# git submodule update --init --recursive
+# scripts/install_deps.sh
+# mkdir build
+# cd build
+# cmake .. && make
+# # wget https://github.com/ethereum/solidity/releases/download/v0.4.19/solc-static-linux
+# sudo mv solc/solc /usr/local/bin/
+# sudo chmod 0755 /usr/local/bin/solc
+# cd ../..
 # && rm -rf $DIR_NAME.tar.gz $DIR_NAME
 
 # install golang
@@ -48,7 +48,7 @@ echo 'PATH=$PATH:/usr/local/go/bin' >> /home/ubuntu/.bashrc
 
 # make/install quorum
 RELEASE="v1.2.1-modified"
-git clone https://github.com/Eximchain/quorum.git
+git clone https://github.com/andreweximchain/quorum.git
 pushd quorum >/dev/null
 git checkout $RELEASE
 make all
